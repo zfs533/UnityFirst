62ec7c323e404359e2e9a071153fbe79c1aad0be
zfs533 <2415933434@qq.com>
2015-07-01 22:27:50 +0800

    test house

---
+++ unitypractice/Assets/csript/StandardAssets/ControlGui_JS.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+function Start () {}
+function Update () {}
+
+
+public function callMe(test : String)
+{
+	print(test);
+}
+
+public function OnGUI()
+{
+	GUILayout.Space(50);
+	if ( GUILayout.Button("JavaScript调用JavaScript") )
+	{
+		//gameObject.SendMessage("showGui");
+		UIText.isGui = true;
+	}
+	
+	
+}
--- unitypractice/Assets/csript/scene03/UIText.js
+++ unitypractice/Assets/csript/scene03/UIText.js
@@ -1,23 +1,30 @@
 ﻿#pragma strict
+public static var isGui : boolean = false;
+function Start () {}
+function Update () {}
 
-function Start () 
+public function showGui()
 {
-	
+	isGui = true;
 }
 
-function Update () {
-
+public function hidGui()
+{
+	isGui = false;
 }
 
 public function OnGUI()
 {
-	GUILayout.BeginVertical();
-	GUILayout.Space(100);
-	GUILayout.BeginHorizontal();
-	GUILayout.Space(100);
-	GUILayout.Button("小润么么哒");
-	GUILayout.Space(100);
-	GUILayout.Label("小优么么哒");
-	GUILayout.EndHorizontal();
-	GUILayout.EndVertical();
+	if ( isGui )
+	{
+		GUILayout.BeginVertical();
+		GUILayout.Space(100);
+		GUILayout.BeginHorizontal();
+		GUILayout.Space(100);
+		GUILayout.Button("小润么么哒");
+		GUILayout.Space(100);
+		GUILayout.Label("小优么么哒");
+		GUILayout.EndHorizontal();
+		GUILayout.EndVertical();
+	}
 }
